cb12f4eb517454f0f0e3b5098d703e27d8b64a07
Josh Thoo Jen Sen <No public email provided>
2023-10-21 22:37:32 +0800

    Changes made from Source Academy

---
+++ Studio/W10_bubblesort_linkedlist.js
@@ -0,0 +1,38 @@
+function bubblesort_list(L) {
+    if (is_null(L) || is_null(tail(L))) {
+        return L;
+    }
+    
+    let list_end = L;
+    //initialisation for list end to be at last element
+    while (!is_null(list_end)) {
+        list_end = tail(list_end);
+    }
+    
+    let prev_index = L;
+    let next_index = tail(L);
+    while (list_end !== L) {
+        while (next_index !== list_end) {
+            if (head(prev_index) > head(next_index)) {
+                const temp = head(next_index);
+                set_head(next_index, head(prev_index));
+                set_head(prev_index, temp);
+            }
+            
+            //move pair of iterators forward in the list
+            prev_index = next_index;
+            next_index = tail(next_index);
+        }
+        //advance list end forward
+        list_end = prev_index;
+        
+        //reset pair of iterators through list
+        prev_index = L;
+        next_index = tail(L);
+    }
+    
+}
+
+const LL = list(3, 5, 2, 4, 1); 
+bubblesort_list(LL);
+LL;
